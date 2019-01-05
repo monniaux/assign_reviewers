ae7d2fcadfa06ae32ef01ae38996aed9332ac410
David Monniaux <david.monniaux@univ-grenoble-alpes.fr>
2019-01-05 12:21:33 +0100

    hop

--- attributions.py
+++ attributions.py
@@ -1,27 +1,60 @@
+#!/usr/bin/python3
+
 import random
 random.seed("Christelle")
 
 nr_dossiers = 142
-nr_examinateurs = 8
-nr_examinateurs_par_dossier = 2
+nr_rapporteurs = 8
+nr_rapporteurs_par_dossier = 2
 
-nr_dossiers_par_examinateur_base = (nr_dossiers*nr_examinateurs_par_dossier) / nr_examinateurs
-examinateurs_avec_extra = (nr_dossiers*nr_examinateurs_par_dossier) % nr_examinateurs
+nr_dossiers_par_rapporteur_base = (nr_dossiers*nr_rapporteurs_par_dossier) // nr_rapporteurs
+rapporteurs_avec_extra = (nr_dossiers*nr_rapporteurs_par_dossier) % nr_rapporteurs
 
-nr_dossiers_pour_examinateurs = [((nr_dossiers_par_examinateur_base+1) if examinateur < examinateurs_avec_extra else nr_dossiers_par_examinateur_base) for examinateur in range(nr_examinateurs)]
+nr_dossiers_pour_rapporteurs = [((nr_dossiers_par_rapporteur_base+1) if rapporteur < rapporteurs_avec_extra else nr_dossiers_par_rapporteur_base) for rapporteur in range(nr_rapporteurs)]
 while True:
     try:
-        disponibilites = list(nr_dossiers_pour_examinateurs)
+        disponibilites = list(nr_dossiers_pour_rapporteurs)
         choix=[]
         for dossier in range(nr_dossiers):
-            examinateurs_disponibles = [i for i in range(nr_examinateurs) if disponibilites[i]>0]
-            examinateurs = random.sample(examinateurs_disponibles, nr_examinateurs_par_dossier)
-            for i in examinateurs:
+            rapporteurs_disponibles = [i for i in range(nr_rapporteurs) if disponibilites[i]>0]
+            rapporteurs = random.sample(rapporteurs_disponibles, nr_rapporteurs_par_dossier)
+            for i in rapporteurs:
                 disponibilites[i] -= 1
-            choix.append(examinateurs)
+            choix.append(rapporteurs)
     except ValueError:
         pass
     else:
         break
 
-print(choix)
+assignation = [[] for i in range(nr_rapporteurs)]
+for i in range(nr_dossiers):
+    for j in choix[i]:
+        assignation[j].append(i)
+        
+from openpyxl import Workbook
+wb = Workbook()
+ws_candidats = wb.active
+ws_candidats.title = "candidats"
+
+row=["Nom", "Prénom"]
+for j in range(nr_rapporteurs_par_dossier):
+    row.append("rapport %d"%(j+1))
+ws_candidats.append(row)
+
+for i in range(nr_dossiers):
+    row = ["nom%03d"%(i+1), "prénom%03d"%(i+1)]
+    for j in range(nr_rapporteurs_par_dossier):
+        row.append("=rapporteurs!A%d" % (choix[i][j]+2))
+    ws_candidats.append(row)
+    
+ws_rapporteurs = wb.create_sheet("rapporteurs")
+row=["Nom"]
+ws_rapporteurs.append(row)
+for i in range(nr_rapporteurs):
+    row = ["rapporteur%02d"%(i+1)]
+    for candidat in assignation[i]:
+        row.append("=candidats!A%d" % (candidat+2))
+        row.append("=candidats!B%d" % (candidat+2))
+    ws_rapporteurs.append(row)
+
+wb.save("rapporteurs.xlsx")
